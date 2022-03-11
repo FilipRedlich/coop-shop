040a314b97ba3c10758fae8530adfa5c48a8cca7
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-11 18:06:51 +0100

    admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Choice, Question
+from .models import Users,Basket,Products
 
-
+'''
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
@@ -16,3 +16,4 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
+'''
