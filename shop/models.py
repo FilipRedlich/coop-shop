cd05c2b0f9fe7f1436555c45a9e315c7945c08b3
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-11 18:02:29 +0100

    model test

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,24 @@
+from operator import mod
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
 
+class Users(models.Model):
+    login=models.CharField(max_length=30)
+    password=models.CharField(max_length=200)
+
+class Basket(models.Model):
+    userID=models.ForeignKey(Users, on_delete=models.CASCADE)
+    productID=models.IntegerField()
+
+class Products(models.Model):
+    name=models.CharField(max_length=200)
+    quantity=models.IntegerField()
+    price=models.FloatField()
+    rating=models.FloatField(default=3.0)
+
+'''
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -11,3 +28,4 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+'''
