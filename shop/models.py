606c8430b58d4c7d685eec05578ea5248d83a3e3
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-11 18:16:20 +0100

    admin db

--- shop/models.py
+++ shop/models.py
@@ -6,14 +6,19 @@ from django.db import models
 
 class Users(models.Model):
     login=models.CharField(max_length=30)
-    password=models.CharField(max_length=200)
+    password=models.CharField(max_length=255)
 
 class Basket(models.Model):
     userID=models.ForeignKey(Users, on_delete=models.CASCADE)
     productID=models.IntegerField()
 
+class Categories(models.Model):
+    name=models.CharField(max_length=50)
+
 class Products(models.Model):
-    name=models.CharField(max_length=200)
+    name=models.CharField(max_length=255)
+    picture=models.CharField(max_length=255)
+    cat=models.ForeignKey(Categories, on_delete=models.CASCADE)
     quantity=models.IntegerField()
     price=models.FloatField()
     rating=models.FloatField(default=3.0)
--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
-from .models import Users,Basket,Products
+from .models import Users,Basket,Categories,Products
 
 
 class IndexView(generic.ListView):
