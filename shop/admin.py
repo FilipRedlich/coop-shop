7832e5b5160ffe92b311cb029be499f950824a5a
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-12 14:59:32 +0100

    Removed useless code

--- shop/admin.py
+++ shop/admin.py
@@ -22,19 +22,3 @@ admin.site.register(Products,ProductsAdmin)
 admin.site.register(Users,UsersAdmin)
 admin.site.register(Basket,BasketAdmin)
 admin.site.register(Categories,CategoriesAdmin)
-
-'''
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
-
-admin.site.register(Question, QuestionAdmin)
-'''
