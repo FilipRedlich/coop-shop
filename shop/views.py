273be2bfcc2024cb611c0ac9c9136b19983de113
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-14 15:09:40 +0100

    db insert test

--- shop/views.py
+++ shop/views.py
@@ -18,6 +18,9 @@ class CatView(TemplateView):
     template_name = 'shop/build/index.html'
     #context_object_name = 'cat_list'
 
+    #insert to db
+    #Categories.objects.create(name="test")
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['cats'] = Categories.objects.all()
