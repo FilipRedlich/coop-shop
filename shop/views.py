ed684e5c2de07900b145f9349bb4ab3c4561f98c
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-17 22:40:07 +0100

    add func in tags

--- shop/views.py
+++ shop/views.py
@@ -18,9 +18,6 @@ class CatView(TemplateView):
     template_name = 'shop/build/index.html'
     #context_object_name = 'cat_list'
 
-    #insert to db
-    #Categories.objects.create(name="test")
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Categories.objects.all()
@@ -28,58 +25,12 @@ class CatView(TemplateView):
         context['users'] = Users.objects.all()
         context['basket'] = Basket.objects.all()
         return context
-    
-    #def get_queryset(self):
-    #    return Categories.objects.order_by('name')
 
-    '''
-    context_object_name = 'latest_question_list'
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-    '''
-
+'''
 def insert_into_cat(name):
     Categories.objects.create(name=name)
 
-def get_related_deltas(self,epk):
-    mystring = ""
-    if not self.get_error_code_delta(epk):
-        return mystring
-    else:
-        for i in self.get_listof_outage():
-            item = i.error_code.all()
-            for x in item:
-                if epk == x.id:
-                    mystring= mystring +" "+str(i.delta())
-    return mystring  
-'''
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'shop/detail.html'
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'shop/results.html'
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'shop/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('shop:results', args=(question.id,)))
+def register(login,password):
+    Users.objects.create(login=login,password=sha256(password))
 '''
