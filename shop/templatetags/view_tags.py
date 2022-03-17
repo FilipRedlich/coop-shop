75b5a0c7413fb6c98efec9036bf78a2e06ddfd94
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-17 22:40:07 +0100

    add func in tags

--- shop/templatetags/view_tags.py
+++ shop/templatetags/view_tags.py
@@ -1,9 +1,13 @@
+from hashlib import sha256
 from django import template
-
-from shop.views import insert_into_cat
+from shop.models import Categories, Users
 
 register = template.Library()
-
 @register.simple_tag
-def insert_into_cat_tag(name):
-    insert_into_cat(name)
+
+def insert_into_cat(name):
+    return 0
+    Categories.objects.create(name=name)
+
+def registerUser(login,password):
+    Users.objects.create(login=login,password=sha256(password))
