09828bd0761ff37649cdaeb7772f967106d23955
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-20 13:13:17 +0100

    turned off testing inserts into db

--- shop/templatetags/view_tags.py
+++ shop/templatetags/view_tags.py
@@ -7,14 +7,10 @@ register = template.Library()
 @register.simple_tag
 #insert into cat db
 def insert_into_cat(name):
-    #turned off for testing
-    return 0
     Categories.objects.create(name=name)
 
 @register.simple_tag
 #adds new user to db
 def registerUser(login,password):
-    #turned off for testing
-    return 0
     passT = sha256(password.encode('utf-8'))
     Users.objects.create(login=login,password=passT.hexdigest())
