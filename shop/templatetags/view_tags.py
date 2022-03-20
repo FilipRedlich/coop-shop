12fb1b1aee927b43831d894ea305ab4cbf7901f0
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-20 13:01:27 +0100

    added register test

--- shop/templatetags/view_tags.py
+++ shop/templatetags/view_tags.py
@@ -3,14 +3,18 @@ from django import template
 from shop.models import Categories, Users
 
 register = template.Library()
-@register.simple_tag
 
+@register.simple_tag
 #insert into cat db
 def insert_into_cat(name):
     #turned off for testing
     return 0
     Categories.objects.create(name=name)
 
+@register.simple_tag
 #adds new user to db
 def registerUser(login,password):
-    Users.objects.create(login=login,password=sha256(password))
+    #turned off for testing
+    return 0
+    passT = sha256(password.encode('utf-8'))
+    Users.objects.create(login=login,password=passT.hexdigest())
