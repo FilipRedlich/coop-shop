e18a07c44e80e2167947bbe90fb8cf4709109a94
Bartosz Skrzydelski <73198283+BrtSkr@users.noreply.github.com>
2022-03-26 23:53:37 +0100

    Update

---
+++ shop/templates/shop/src/js/Components/Footer.js
@@ -0,0 +1,16 @@
+const MainFooter = () => {
+    return (
+      
+        <footer className="d-flex flex-row flex-wrap bg-transparent-custom w-100 text-white">
+          <div className="">
+            <h2>LINKS</h2>
+            <h5>Github</h5>
+            <h5>Github</h5>
+            <h5>Github</h5>
+          </div>
+        </footer>
+      
+    );
+}
+
+export default MainFooter;
