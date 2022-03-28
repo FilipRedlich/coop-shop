7ec7dd486710af5a4a77cfe9313b5cebed093551
Bartosz Skrzydelski <73198283+BrtSkr@users.noreply.github.com>
2022-03-28 15:55:54 +0200

    Update

--- shop/templates/shop/src/js/Components/Footer.js
+++ shop/templates/shop/src/js/Components/Footer.js
@@ -8,15 +8,15 @@ const MainFooter = () => {
           <div className="d-flex flex-row gap-5 flexb-30">
             <div className="">
               <h3 className="text-uppercase">links</h3>
-              <h6><a className="text-muted remove-all-decoration" href="https://github.com/FilipRedlich/coop-shop">Source code</a></h6>
-              <h6><a className="text-muted remove-all-decoration" href="https://github.com/BrtSkr">BrtSkr</a></h6>
-              <h6><a className="text-muted remove-all-decoration" href="https://github.com/FilipRedlich">FilipRedlich</a></h6>
+              <h6><a className="footer-anchor remove-all-decoration" href="https://github.com/FilipRedlich/coop-shop">Source code</a></h6>
+              <h6><a className="footer-anchor remove-all-decoration" href="https://github.com/BrtSkr">BrtSkr</a></h6>
+              <h6><a className="footer-anchor remove-all-decoration" href="https://github.com/FilipRedlich">FilipRedlich</a></h6>
             
             </div>
             <div className="">
               <h3 className="text-uppercase">contact</h3>
-              <h6><a className="text-muted remove-all-decoration" href="mailto:arcadam111@gmail.com">BrtSkr</a></h6>
-              <h6><a className="text-muted remove-all-decoration" href="mailto:placeholder">FilipRedlich</a></h6>
+              <h6><a className="footer-anchor remove-all-decoration" href="mailto:arcadam111@gmail.com">BrtSkr</a></h6>
+              <h6><a className="footer-anchor remove-all-decoration" href="mailto:placeholder">FilipRedlich</a></h6>
               <h6></h6>
             </div>
           </div>
