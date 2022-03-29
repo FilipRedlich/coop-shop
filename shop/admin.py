ebbbf76b30113cec8cef438da3ba1ca1170a09cb
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-29 17:34:59 +0200

    admin view

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Users,Basket,Categories,Products
+from .models import Users,Basket,Categories,Products,subCategories
 
 class ProductsAdmin(admin.ModelAdmin):
     #fields = ['name','picture','cat','quantity','price','mult','rating']
@@ -18,7 +18,12 @@ class CategoriesAdmin(admin.ModelAdmin):
     #fields = ['name']
     list_display=('name',)
 
+class subCategoriesAdmin(admin.ModelAdmin):
+    #fields = ['name']
+    list_display=('catID','prodID')
+
 admin.site.register(Products,ProductsAdmin)
 admin.site.register(Users,UsersAdmin)
 admin.site.register(Basket,BasketAdmin)
 admin.site.register(Categories,CategoriesAdmin)
+admin.site.register(subCategories,subCategoriesAdmin)
