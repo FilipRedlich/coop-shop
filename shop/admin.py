e66aa65e91f297e8d363b8765fdb6c868e928b74
FilipRedlich <filipredlich.pr@gmail.com>
2022-03-31 13:47:20 +0200

    added services tab

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Users,Basket,Categories,Products,subCategories
+from .models import Services,Users,Basket,Categories,Products,subCategories
 
 class ProductsAdmin(admin.ModelAdmin):
     #fields = ['name','picture','cat','quantity','price','mult','rating']
@@ -22,8 +22,12 @@ class subCategoriesAdmin(admin.ModelAdmin):
     #fields = ['name']
     list_display=('name','catID')
 
+class servicesAdmin(admin.ModelAdmin):
+    list_display=('name','price')
+
 admin.site.register(Products,ProductsAdmin)
 admin.site.register(Users,UsersAdmin)
 admin.site.register(Basket,BasketAdmin)
 admin.site.register(Categories,CategoriesAdmin)
 admin.site.register(subCategories,subCategoriesAdmin)
+admin.site.register(Services,servicesAdmin)
