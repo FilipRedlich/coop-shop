21bd5fab4f8f43702f5819dc178b07a48621fc38
FilipRedlich <filipredlich.pr@gmail.com>
2022-04-01 16:19:40 +0200

    one more test

--- shop/urls.py
+++ shop/urls.py
@@ -6,6 +6,7 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('test/', views.TestView.as_view(), name='test'),
+    path('test2/', views.TestView2.as_view(), name='tes2t'),
 #    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 #    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
 #    path('<int:question_id>/vote/', views.vote, name='vote'),
