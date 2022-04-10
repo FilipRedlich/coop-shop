4c0c50fa9248362e16971eaadf4270d09dc028ca
FilipRedlich <filipredlich.pr@gmail.com>
2022-04-10 18:30:00 +0200

    test register

--- shop/urls.py
+++ shop/urls.py
@@ -5,9 +5,7 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('register/', views.register, name='register'),
     path('test/', views.TestView.as_view(), name='test'),
     path('test2/', views.TestView2.as_view(), name='tes2t'),
-#    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-#    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-#    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
