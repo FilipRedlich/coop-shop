050adbf7eda2b57eaa81787fcd158ceeb4a80eb2
FilipRedlich <filipredlich.pr@gmail.com>
2022-04-11 21:52:17 +0200

    added compression

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+MIDDLEWARE_CLASSES = [
     'django.middleware.gzip.GZipMiddleware',
 ]
 
