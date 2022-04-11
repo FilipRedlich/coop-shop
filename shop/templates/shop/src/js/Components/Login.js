f83d7c19eba83936ccf1f7d9ad162d86f508cdd6
Bartosz Skrzydelski <73198283+BrtSkr@users.noreply.github.com>
2022-04-11 21:53:59 +0200

    Test build

--- shop/templates/shop/src/js/Components/Login.js
+++ shop/templates/shop/src/js/Components/Login.js
@@ -2,66 +2,33 @@ import { render } from "react-dom";
 import { useEffect, useState } from "react";
 import { check } from "prettier";
 
-
-//"Blueprint" for loginSwitch Button
-const RegisterSwitch = () => {
-  return (
-    <input
-      type="submit"
-      className="btn btn-lg text-white w-100 bg-black"
-      value="Register"
-     
-    />
-  );
-};
-//"Blueprint" for loginSwitch Button
-const LoginSwitch = () => {
-  return (
-    
-    <input
-      type="submit"
-      className="btn btn-lg text-white w-100 bg-black"
-      value='Login'
-      
-    
-      
-    />
-  );
-};
-
 const Login = () => {
   const [isLogin, setOpposite] = useState(false);
-  var form = document.querySelector('#login-form')
+  var form = document.querySelector("#login-form");
   //changes element based on useState boolean above (isLogin and setOpposite)
   const checkState = () => {
-
+    const buttonSwitch = document.querySelector("#switch");
+    //if true it changes form attribute to register (and text of input)
     if (isLogin) {
-      render(<RegisterSwitch />, document.querySelector("#buttonSwitch"));
-      form.setAttribute("action", "/register/"); 
+      form.setAttribute("action", "/register/");
+      buttonSwitch.value = "Register";
     }
-
+    //if false it changes form attribute to login
     if (!isLogin) {
-      render(<LoginSwitch />, document.querySelector("#buttonSwitch"));
-      form.setAttribute('action', '/login/')
+      form.setAttribute("action", "/login/");
+      buttonSwitch.value = "Login";
     }
-  }
+  };
   useEffect(() => {
     console.log(isLogin);
-    setTimeout(checkState, 10)
-    
-   // isLogin
-   //   ? render(<RegisterSwitch />, document.querySelector("#buttonSwitch"))
-   //   : null;
-   // !isLogin
-   //   ? render(<LoginSwitch />, document.querySelector("#buttonSwitch"))
-   //   : null;
+    setTimeout(checkState, 10);
   });
   return (
     <>
-      <section className="w-100 bg-dark position-relative min-vh-100 d-flex flex-column">
+      <section className="w-100 bg-dark position-relative login-section d-flex flex-column">
         <div className="d-flex login-switch  justify-content-center mx-auto ">
           <button
-            className="btn w-50 rounded-left-side bg-black text-white border-0 p-2"
+            className="btn action w-50 rounded-left-side bg-black text-white border-0 p-2"
             onClick={(e) => {
               setOpposite(false); //useState hook
               e.preventDefault();
@@ -71,7 +38,7 @@ const Login = () => {
           </button>
           <button
             type="submit"
-            className="btn w-50 rounded-right-side bg-black text-white border-0 p-2"
+            className="btn action w-50 rounded-right-side bg-black text-white border-0 p-2"
             onClick={(e) => {
               setOpposite(true); //useState hook
               e.preventDefault();
@@ -82,7 +49,7 @@ const Login = () => {
         </div>
         <form
           id="login-form"
-          className="d-flex flex-column gap-4 text-white login-modal shadow-lg p-5"
+          className="d-flex flex-column gap-4 text-white login-modal  p-5"
           action="/login/"
           method="POST"
         >
@@ -91,7 +58,7 @@ const Login = () => {
             className="bg-black account-credentials rounded border-0 text-white"
             name="email"
             placeholder="Email"
-            maxLength='30'
+            maxLength="30"
           />
           <input
             className="bg-black account-credentials rounded border-0 text-white"
@@ -101,7 +68,14 @@ const Login = () => {
           />
 
           {/* container forswitched button */}
-          <div id="buttonSwitch"></div>
+          <div id="buttonSwitch">
+            <input
+              id="switch"
+              type="submit"
+              className="btn btn-lg text-white w-100 mt-3 bg-black"
+              value="Login"
+            />
+          </div>
         </form>
       </section>
     </>
