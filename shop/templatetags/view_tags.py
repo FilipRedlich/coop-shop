11cb86e5dcd00852c117b1163787be89d14f9691
FilipRedlich <filipredlich.pr@gmail.com>
2022-04-13 22:50:32 +0200

    cleaned output funcions

--- shop/templatetags/view_tags.py
+++ shop/templatetags/view_tags.py
@@ -19,7 +19,7 @@ def outputSubcat(cat,field):
 #check for discount and returns items from cat with discount
 def outputCat(cat):
     if(ON==1):
-        temp = subCategories.objects.values_list("name").filter(catID=cat)
+        temp = subCategories.objects.values_list('name').filter(catID=cat)
         out=""
         for t in temp:
             out+=str(t)
@@ -43,3 +43,29 @@ def outputBasket(userID):
         for tt in getUserProds:
             tab += str(tt[2])+" "
         return tab
+
+@register.simple_tag
+def getSubcatsFromCat(cat,field):
+    if(ON==1):
+        temp = subCategories.objects.values_list().filter(catID=cat)
+        out=""
+        i=0
+        for t in temp:
+            if(i!=0):
+                out+=";"
+            out+=str(t[int(field)])
+            i=1
+        return out
+
+@register.simple_tag
+def getProductsFromSubcat(cat,field):
+    if(ON==1):
+        temp = Products.objects.values_list().filter(subcat=cat)
+        out=""
+        i=0
+        for t in temp:
+            if(i!=0):
+                out+=";"
+            out+=str(t[int(field)])
+            i=1
+        return out
