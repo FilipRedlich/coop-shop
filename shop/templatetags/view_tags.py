559b9cedc30e3743c60604db22d05bfb894f29a0
FilipRedlich <filipredlich.pr@gmail.com>
2022-04-16 17:48:17 +0200

    added comments and prepared for change functions

--- shop/templatetags/view_tags.py
+++ shop/templatetags/view_tags.py
@@ -3,11 +3,13 @@ from shop.models import Basket, Categories, Products, Users, subCategories
 
 register = template.Library()
 
+#change to 0 after refractor
 ON=1
 
 @register.simple_tag
-#check for discount and returns items from cat with discount
+#output all products specified field from selected subcat
 def outputSubcat(cat,field):
+    #redundant function but still on to maintain site until refractor
     if(ON==1):
         temp = Products.objects.values_list(field).filter(subcat=cat)
         out=""
@@ -16,8 +18,9 @@ def outputSubcat(cat,field):
         return out
 
 @register.simple_tag
-#check for discount and returns items from cat with discount
+#output all subcats from cat
 def outputCat(cat):
+    #redundant function but still on to maintain site until refractor
     if(ON==1):
         temp = subCategories.objects.values_list('name').filter(catID=cat)
         out=""
@@ -26,46 +29,49 @@ def outputCat(cat):
         return out
 
 @register.simple_tag
-#check for discount and returns items from cat with discount
+#check for discount and returns items from subcat with discount
 def hasDiscount(cat):
-    if(ON==1):
-        temp = Products.objects.values_list("name","price","mult").filter(subcat=cat).exclude(mult=1.0)
-        out=""
-        for t in temp:
-            out+=str(t)
-        return out
+    query = Products.objects.values_list("name","price","mult").filter(subcat=cat).exclude(mult=1.0)
+    out=""
+    for row in query:
+        out+=str(row)
+    return out
 
 @register.simple_tag
+#output all products from selected user basket
 def outputBasket(userID):
-    if(ON==1):
-        getUserProds = Basket.objects.values_list().filter(userID=userID)
-        tab = ""
-        for tt in getUserProds:
-            tab += str(tt[2])+" "
-        return tab
+    query = Basket.objects.values_list().filter(userID=userID)
+    tab = ""
+    i=0
+    for row in query:
+        if(i!=0):
+            out+=";"
+        out+=str(row[2])
+        i=1
+    return tab
 
 @register.simple_tag
+#output all subcats specified field from selected cat
 def getSubcatsFromCat(cat,field):
-    if(ON==1):
-        temp = subCategories.objects.values_list().filter(catID=cat)
-        out=""
-        i=0
-        for t in temp:
-            if(i!=0):
-                out+=";"
-            out+=str(t[int(field)])
-            i=1
-        return out
+    query = subCategories.objects.values_list().filter(catID=cat)
+    out=""
+    i=0
+    for row in query:
+        if(i!=0):
+            out+=";"
+        out+=str(row[int(field)])
+        i=1
+    return out
 
 @register.simple_tag
+#output all products specified field from selected subcat
 def getProductsFromSubcat(cat,field):
-    if(ON==1):
-        temp = Products.objects.values_list().filter(subcat=cat)
-        out=""
-        i=0
-        for t in temp:
-            if(i!=0):
-                out+=";"
-            out+=str(t[int(field)])
-            i=1
-        return out
+    query = Products.objects.values_list().filter(subcat=cat)
+    out=""
+    i=0
+    for row in query:
+        if(i!=0):
+            out+=";"
+        out+=str(row[int(field)])
+        i=1
+    return out
