aebe53c3c233031c1bd1fd98dd51b59f68bef113
FilipRedlich <filipredlich.pr@gmail.com>
2022-04-24 20:00:23 +0200

    tested adding to basket

--- shop/views.py
+++ shop/views.py
@@ -130,8 +130,7 @@ def logout(request):
 def addProductToBasket(request):
     #getting user id to identify user
     productID = request.POST['id']
-    productID = request.session['userpk']
     #query to add product to given user's basket
-    Basket.objects.create(userID=request.session['userpk'],productID=productID)
+    Basket.objects.create(userID_id=request.session['userpk'],productID_id=productID)
     #redirect to index after end of function
     return HttpResponseRedirect(reverse('shop:index'))
