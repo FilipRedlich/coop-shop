1a1f7d772fe9c97857199a0b0983ced6c1173b74
FilipRedlich <filipredlich.pr@gmail.com>
2022-04-28 21:58:54 +0200

    added product view

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
     path('', views.indexView, name='index'),
+    path('product/', views.productView, name='product'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
