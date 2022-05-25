f25b7b0cf029237e9bcbc34b107bc8e0713842ad
FilipRedlich <filipredlich.pr@gmail.com>
2022-05-25 12:27:18 +0200

    added ability to set quantity when adding product to basket

--- shop/views.py
+++ shop/views.py
@@ -191,8 +191,13 @@ def logout(request):
 def addProductToBasket(request):
     #getting user id to identify user
     productID = request.POST['id']
+    #getting quantity to set or =1 if not set
+    if request.POST['quantity']:
+        quantity = request.POST['id']
+    else:
+        quantity = 1
     #query to add product to given user's basket
-    Basket.objects.create(userID_id=request.session['userpk'],productID_id=productID)
+    Basket.objects.create(userID_id=request.session['userpk'],productID_id=productID,quantity=quantity)
     #redirect to index after end of function
     return HttpResponseRedirect(reverse('shop:index'))
 
