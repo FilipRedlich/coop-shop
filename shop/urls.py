0d0f4d8d5d5b00a226ae9cd944ba68b5cdbc1c59
FilipRedlich <filipredlich.pr@gmail.com>
2022-05-26 21:04:52 +0200

    added skeleton of subscribeToNewsletter

--- shop/urls.py
+++ shop/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('addProductToBasket/', views.addProductToBasket, name='addProductToBasket'),
     path('deleteProductToBasket/', views.deleteProductToBasket, name='deleteProductToBasket'),
     path('changeHotshot/', views.changeHotshot, name='changeHotshot'),
+    path('subscribeToNewsletter/', views.subscribeToNewsletter, name='subscribeToNewsletter'),
     path('test/', views.testView, name='test'),
     path('test2/', views.testView2, name='test2'),
     path('test3/', views.testView3, name='test3'),
