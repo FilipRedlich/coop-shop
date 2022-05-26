48dc9d7a3995e7b115f897eb28afebd49d2feab3
FilipRedlich <filipredlich.pr@gmail.com>
2022-05-26 21:11:14 +0200

    changed default subscription to newsletter

--- shop/models.py
+++ shop/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class Users(models.Model):
     login=models.CharField(max_length=30)
     password=models.CharField(max_length=255)
-    newsletter=models.BooleanField(default=True)
+    newsletter=models.BooleanField(default=False)
     def __str__(self):
         return str(self.pk)+". "+self.login
 
