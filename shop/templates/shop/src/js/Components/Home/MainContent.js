a8eb0ae771c3b5d6817b3ed8112e4210df0b771a
Bartosz Skrzydelski <73198283+BrtSkr@users.noreply.github.com>
2022-08-10 20:11:13 +0200

    Bug fix

--- shop/templates/shop/src/js/Components/Home/MainContent.js
+++ shop/templates/shop/src/js/Components/Home/MainContent.js
@@ -7,7 +7,7 @@ const MainContent = () => {
     <article className="container-fluid main py-5 bg-dark text-white  animate__animated animate__backInRight">
       <section className="main-content">
         <Carousel />
-        <Hotshot info={hotshotTest} />
+        <Hotshot info={hotshot} />
       </section>
       <ItemBar />
     </article>
